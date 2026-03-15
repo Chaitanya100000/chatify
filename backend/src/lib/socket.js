25aa30a54ed0a99f50b4be9ae0620e77be2887c5
Chaitanya100000 <chaitanyatakle60@gmail.com>
2026-03-15 14:21:09 +0530

    fix socket config and production API url

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,52 +1,3 @@
-// import { Server } from "socket.io";
-// import http from "http";
-// import express from "express";
-// import { ENV } from "./env.js";
-// import { socketAuthMiddleware } from "../middleware/socket.auth.middleware.js";
-
-// const app = express();
-// const server = http.createServer(app);
-
-// const io = new Server(server, {
-//   cors: {
-//     origin: [ENV.CLIENT_URL],
-//     credentials: true,
-//   },
-// });
-
-// // apply authentication middleware to all socket connections
-// io.use(socketAuthMiddleware);
-
-// // we will use this function to check if the user is online or not
-// export function getReceiverSocketId(userId) {
-//   return userSocketMap[userId];
-//   // ?.[0];
-// }
-
-// // this is for storig online users
-// const userSocketMap = {}; // {userId: [socketId]}
-
-// io.on("connection", (socket) => {
-//   console.log("A user connected", socket.user.fullName);
-
-//   const userId = socket.userId;
-//   userSocketMap[userId] = socket.id;
-
-//   // io.emit() is used to send events to all connected clients
-//   io.emit("getOnlineUser", Object.keys(userSocketMap));
-
-//   // with socket.on we listen for events from clients
-//   socket.on("disconnect", () => {
-//     console.log("A user disconnected", socket.user.fullName);
-//     delete userSocketMap[userId];
-//     io.emit("getOnlineUser", Object.keys(userSocketMap));
-//   });
-// });
-
-// export { io, app, server };
-
-
-
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
@@ -58,7 +9,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: [ENV.CLIENT_URL],
+    origin: ENV.CLIENT_URL,
+    methods: ["GET", "POST"],
     credentials: true,
   },
 });
@@ -89,7 +41,7 @@ io.on("connection", (socket) => {
 
     if (userSocketMap[userId]) {
       userSocketMap[userId] = userSocketMap[userId].filter(
-        (id) => id !== socket.id
+        (id) => id !== socket.id,
       );
 
       if (userSocketMap[userId].length === 0) {
