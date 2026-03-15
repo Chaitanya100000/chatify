f0fff2298a97e7b12d131f680a4ec7ab69e1f47a
Chaitanya100000 <chaitanyatakle60@gmail.com>
2026-03-15 15:20:33 +0530

    fix socket connection url

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -37,12 +37,6 @@ export const signup = async (req, res) => {
     });
 
     if (newUser) {
-      // before CR:
-      // generateToken(newUser._id, res);
-      // await newUser.save();
-
-      // after CR:
-      // Persist user first, then issue auth cookie
       const savedUser = await newUser.save();
       generateToken(savedUser._id, res);
 
